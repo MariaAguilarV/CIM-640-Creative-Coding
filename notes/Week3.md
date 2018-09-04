33eb1a7a8e4678b01b498a8d22d88bb03e2c6e41
MariaAguilarV <MariaAguilarV@users.noreply.github.com>
2018-09-04 18:14:07 -0400

    week3 - first class

---
+++ notes/Week3.md
@@ -0,0 +1,16 @@
+projection mapping - mad mapper is a software
+
+-- Continue ClassExampleDraw
+1. point is a representation of a single pixel
+point(width/2, height/2); -> using width and height allows changing the canvas
+2. stroke is the outline that round the shape.
+  Stroke acts as a switched. When the program starts, the stroke is black, then the switch turn on to red. if we want to change the stroke to white, then we have to specify it.
+  3. IN p5js / references, i can find stroke and more functions fore stroke
+4. fill is used to fill color. Stroke is for borders
+
+#### variables
+5. We want our draws scale with our screen size, using a variable
+6. a variable is an alphanumeric representation of different things.
+7. to declare variables we use 'var'
+8. declare variables above the setup. declaring at the top at the program, means they are global variables. If I declare a variable in the setup, it is only available in the setup.
+9. is good to comment your code!
--- notes/classExampleDraw/sketch.js
+++ notes/classExampleDraw/sketch.js
@@ -1,3 +1,10 @@
+var a = 1;
+var num1 = 3;
+var b = 'b'
+var word = "word";
+var trueFalse = true; //camelcase: start with lowercasse, it is case sensitive
+var sentences = "I am a sentence";
+
 function setup() {
   // put setup code here
   createCanvas(400,400);
@@ -5,12 +12,37 @@ function setup() {
   background(0,0,255);
   background("orange");
   background ("#333363");
+
+  console.log(a+num1); // tell you what your variables are doing trhoughout a program
+  console.log(a-num1);
+  console.log(a+b); //add a number and a letter, it concatenates
+  console.log(a*b); // Error: NaN not a number
+
+  //this is only in setup scope
+  var setupVariable = 5;
+
 }
 
 function draw() {
   // put drawing code here
+  stroke ('black');
+  strokeWeight(num1);
+
+  fill('orange');
+  // eyes
   ellipse(120,120,50,50);
   ellipse (180,120,50,50);
+  //mouth
   rect (120,220,50,20);
 
+//mole
+  stroke('red');
+  strokeWeight(5);
+  point(width/2, height/2);
+
+  line(120,230,170,230);
+
+  //pacperson
+  arc(50,200,100,100,QUARTER_PI, PI + HALF_PI + QUARTER_PI);
+  arc(50,350,100,100,QUARTER_PI, TWO_PI - QUARTER_PI);
 }
