bc841ae785b77ea5519b90949a4107a6b3498f3c
MariaAguilarV <MariaAguilarV@users.noreply.github.com>
2018-10-16 00:00:43 -0400

    Add Play again button update readme

--- Hw/Midterm/readme.md
+++ Hw/Midterm/readme.md
@@ -1,20 +1,26 @@
 # Title
-Trompita and ballons
+### Let's play with Trompita! 
 
 # Explanation:
 * Why are you making this project? What is your motivation?
-
+The other day I was with my family at a store, when my little nephew and niece
 
 * What are your inputs?
   * Press each heart before they touch the background
-  * Press 'try again' button
+  * Press "try again" button
+  * Press "play again" button
 
 * What are your outputs?
   * Start game: Start animation of the balloon flying away
-  * Game over: Show screen to try again when one heart touches the background
+  * Hearts falling: Show random number of hearts falling from top of screen
+  * Heart touched: Disappear the heart images when the user touches them
+  * Lose game: Show screen of the character sad.
+  * Try again: Show try again button
   * Win game: Start animation when all little hearts disappear
 
+
 * Who is the target audience for your project?
-My target audience are kids from 3 to 7 years old. In this range of ages, kids will understand the logic of the game and also between these ages, kids tend to become bored very soon, thus, this game will be a fun way to keep little children busy while parents are buying! 
+<br> My target audience are kids from 3 to 7 years old. In this range of ages, kids will understand the logic of the game and also between these ages, kids tend to become bored quickly, thus, this game will be a fun way to keep little children busy while parents are buying!
 
 * Where will your project be displayed?
+I would project this game in big screens at family stores. I would use a screen as the one showed above.
--- Hw/Midterm/sketch.js
+++ Hw/Midterm/sketch.js
@@ -1,247 +1,7 @@
-// var frameAmounts = 18;
-// var frameArray=[];
-// var currentFrame=0;
-// var interval = 500; // 2000 means 2 seconds
-// var prevMillis = 0;
-//
-// var heart;
-// var xPos = 300;
-// var yPos = 0;
-// var trigger = false;
-// var speed = 3;
-//
-//
-// var transition1 = true;
-// var play= false;
-// var winner = false;
-// var end = false;
-//
-// var numberOfHearts = 4;
-// var numberOfCatchedHearts = 0;
-// var heartsArray;
-//
-// function preload (){
-//
-//   for (var frames=0; frames < frameAmounts; frames++){
-//     var frameString = "AssetsElephant/" + frames + ".png";
-//     frameArray[frames] = loadImage(frameString);
-//   }
-//   heart = loadImage('AssetsElephant/heart.png');
-//   heartsArray = {
-//                   "heart1":[300,0,15,3,true],
-//                   "heart2":[200,0,17,1,true],
-//                   "heart3":[400,-20,20,3,true],
-//                   "heart4":[400,-0,25,4,true]
-//   }
-// }
-//
-// function setup(){
-//   createCanvas(700,525);
-//
-// }
-//
-// function draw(){
-//
-//   if (transition1 == true && play == false && winner == false){
-//     image (frameArray[currentFrame],0,0);
-//     if (millis() - prevMillis > interval){
-//       currentFrame++;
-//       prevMillis = millis();
-//     }
-//
-//     if (currentFrame == 10){
-//       play=true;
-//       transition1 = false;
-//     }
-//   }
-//
-//   if (transition1 == false && play == true && winner == false){
-//
-//       image (frameArray[10],0,0);
-//       for (var keys in heartsArray){
-//           console.log(keys + " values:" + heartsArray[keys]);
-//           if (heartsArray[keys][4] == true){
-//             image(heart,heartsArray[keys][0], heartsArray[keys][1], heart.width/heartsArray[keys][2], heart.height/heartsArray[keys][2]);
-//              heartsArray[keys][1]+=heartsArray[keys][3];
-//
-//               if (heartsArray[keys][1] > height){
-//                 play = false;
-//                 winner = false;
-//               }
-//           }
-//         }
-//   }
-//
-//   if (transition1 == false && play == false && winner == true && end==false){
-//     image (frameArray[currentFrame],0,0);
-//            if (millis() - prevMillis > interval){
-//              currentFrame++;
-//              prevMillis = millis();
-//            }
-//
-//            if (currentFrame == 17){
-//              end=true;
-//            }
-//   }
-//
-//   if (transition1==false && play==false && winner== true && end==true){
-//     image (frameArray[17],0,0);
-//   }
-//
-//
-//   if (transition1 == false && play == false && winner == false) {
-//     image (frameArray[11],0,0);
-//   }
-//
-// }
-//
-// function mousePressed(){
-//   for (keys in heartsArray){
-//     var heartDist = dist(mouseX, mouseY, heartsArray[keys][0], heartsArray[keys][1]);
-//     if (heartDist < heart.width/heartsArray[keys][2]){
-//       heartsArray[keys][4] = false;
-//       numberOfCatchedHearts++;
-//     }
-//
-//   }
-//
-//   if (numberOfCatchedHearts == numberOfHearts) {
-//     winner = true;
-//     currentFrame = 12;
-//     play = false;
-//   }
-// }
-
-//random numero de corazones
-
-// var frameAmounts = 18;
-// var frameArray=[];
-// var currentFrame=0;
-// var interval = 500; // 2000 means 2 seconds
-// var prevMillis = 0;
-//
-// var heart;
-// var xPos = 300;
-// var yPos = 0;
-// var trigger = false;
-// var speed = 3;
-//
-//
-// var transition1 = true;
-// var play= false;
-// var winner = false;
-// var end = false;
-//
-// var numberOfHearts = 0;
-// var numberOfCatchedHearts = 0;
-// var heartsArray = [];
-//
-// function preload (){
-//
-//   for (var frames=0; frames < frameAmounts; frames++){
-//     var frameString = "AssetsElephant/" + frames + ".png";
-//     frameArray[frames] = loadImage(frameString);
-//   }
-//   heart = loadImage('AssetsElephant/heart.png');
-// }
-//
-// function setup(){
-//   createCanvas(700,525);
-//   numberOfHearts = int(random(1,5));
-//   for (var i=0; i<numberOfHearts; i++){
-//     heartsArray.push(new heartGenerator());
-//   }
-// }
-//
-// function heartGenerator() {
-//   this.x = random(600);
-//   this.y = 0;
-//   this.width = random(5,15);
-//   this.speed = random(1,5);
-//   this.show = true;
-// }
-//
-// function draw(){
-//
-//
-//   if (transition1 == true && play == false && winner == false){
-//     image (frameArray[currentFrame],0,0);
-//     if (millis() - prevMillis > interval){
-//       currentFrame++;
-//       prevMillis = millis();
-//     }
-//
-//     if (currentFrame == 10){
-//       play=true;
-//       transition1 = false;
-//     }
-//   }
-//
-//   if (transition1 == false && play == true && winner == false){
-//
-//       image (frameArray[10],0,0);
-//       for (var i=0; i<heartsArray.length; i++){
-//          console.log("number of hearts:" + numberOfHearts);
-//           console.log(heartsArray[i].x);
-//           if (heartsArray[i].show == true){
-//             image(heart,heartsArray[i].x, heartsArray[i].y, heart.width/heartsArray[i].width, heart.height/heartsArray[i].width);
-//              heartsArray[i].y+=heartsArray[i].speed;
-//
-//               if (heartsArray[i].y > height){
-//                 play = false;
-//                 winner = false;
-//               }
-//           }
-//         }
-//   }
-//
-//   if (transition1 == false && play == false && winner == true && end==false){
-//     image (frameArray[currentFrame],0,0);
-//            if (millis() - prevMillis > interval){
-//              currentFrame++;
-//              prevMillis = millis();
-//            }
-//
-//            if (currentFrame == 17){
-//              end=true;
-//            }
-//   }
-//
-//   if (transition1==false && play==false && winner== true && end==true){
-//     image (frameArray[17],0,0);
-//   }
-//
-//
-//   if (transition1 == false && play == false && winner == false) {
-//     image (frameArray[11],0,0);
-//
-//   }
-//
-// }
-//
-// function mousePressed(){
-//   for (var i=0; i<heartsArray.length; i++){
-//     var heartDist = dist(mouseX, mouseY, heartsArray[i].x, heartsArray[i].y);
-//     if (heartDist < heart.width/heartsArray[i].width){
-//       heartsArray[i].show = false;
-//       numberOfCatchedHearts++;
-//     }
-//
-//   }
-//
-//   if (numberOfCatchedHearts == numberOfHearts) {
-//     winner = true;
-//     currentFrame = 12;
-//     play = false;
-//   }
-// }
-
-//button restart game
-
 var frameAmounts = 18;
 var frameArray=[];
 var currentFrame=0;
-var interval = 500; // 2000 means 2 seconds
+var interval = 450; // 2000 means 2 seconds
 var prevMillis = 0;
 
 var heart;
@@ -337,7 +97,7 @@ function draw(){
 
   if (transition1==false && play==false && winner== true && end==true){
     image (frameArray[17],0,0);
-
+    //ellipse (580,380,80,80);
   }
 
 
@@ -380,6 +140,24 @@ function mousePressed(){
         heartsArray.push(new heartGenerator());
       }
     }
-    //rect (515,450,150,50);
+  }
+
+  if (transition1==false && play==false && winner== true && end==true){
+    var cDist = dist(580, 380, mouseX, mouseY);
+    if (cDist<150){
+      transition1=true;
+      play=false;
+      winner=false;
+      end=false;
+      currentFrame = 0;
+      numberOfHearts = 0;
+      numberOfCatchedHearts = 0;
+      heartsArray = [];
+
+      numberOfHearts = int(random(1,5));
+      for (var i=0; i<numberOfHearts; i++){
+        heartsArray.push(new heartGenerator());
+      }
+    }
   }
 }
