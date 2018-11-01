dda2d3b6b84da63b88d3fef1c57717b8ed5024b8
MariaAguilarV <MariaAguilarV@users.noreply.github.com>
2018-11-01 17:07:11 -0400

    Week 10

--- notes/Week5.md
+++ notes/Week5.md
@@ -2,4 +2,13 @@
 2. mousepressed function: when you press and release, it can run a piece of code
 3. if you want to change how the image are draw, you use imageMode
 
-When comparte always use ==
+When compare always use ==
+
+OCTOBER 30
+
+LIBRARIES:
+
+collide - if you have a rectangle and a circle hits the rectangle, the entire boundary. It understands the geometry of the cirle and see if smt is hitting the broder.
+Is used for games
+
+rita.js: language processing. 
--- notes/interfaceFunction/sketch.js
+++ notes/interfaceFunction/sketch.js
@@ -3,45 +3,77 @@ var posY = 0;
 var rectSize = 100;
 var colors = "green";
 
+var button1;
+var button2;
+
+var buttonArray= [];
+
 function setup() {
   createCanvas (400,400);
+  button1 = new interfaces(100,200,50,"red", "Red Button");
+  button2 = new interfaces(200,200,50, "green", "Green Button");
 
+  for (var i=0; i<100; i++){
+    var c = color(random(255), random(255), random(255));
+    buttonArray[i] = new interfaces(random(width), random (height), 50,c,i);
+  }
 }
 
 function draw() {
     // display();
     background(255);
     // display(random(width), random(height), random(255), random(0,100));
-    display(posX, posY, "red", rectSize);
-    var box1 = check(mouseX, mouseY, posX, posY, rectSize);
-    if (box1==true){
-      background ("green");
+    button1.display();
+    if (button1.check(mouseX, mouseY)){
+      console.log(button1.name);
     }
+    button2.display();
+    button2.check(mouseX, mouseY);
 
-    display(posX+100, posY+100, "green", rectSize);
-    var box2 = check(mouseX, mouseY,posX+100, posY+100, rectSize);
-    if (box2 == true){
-      background("red");
+    for (var i=0; i<buttonArray.length; i++){
+      buttonArray[i].display();
+      if (buttonArray[i].check(mouseX, mouseY)){
+        console.log(buttonArray[i].name);
+        background(buttonArray[i].color);
+      }
+
     }
 }
 
-
-
-// function display(){
-//   fill(colors);
-//   ellipse(posX,posY,rectSize,rectSize);
-// }
-
-function display(tempX, tempY, tempC, tempS){
-  fill(tempC);
-  ellipse(tempX,tempY,tempS,tempS);
-}
-
-function check(mX, mY, locX, locY, tempRsize){
-  if (mX> locX && mX< locX + tempRsize && mY>locY && mY<locY + tempRsize){
-    console.log("in box");
-    return true;
-  } else{
-    return false;
+class interfaces{
+  //constructor are the characteristics of the object, it contains the minimun parameters for this object to exist
+  // reccomendation: always create the constructor and then display, then start creating other functions
+  constructor(tempX,tempY, tempS, tempC, tempName){
+    this.x = tempX;
+    this.y = tempY;
+    this.size = tempS;
+    this.color = tempC;
+    this.name = tempName;
+    this.overlay = false;
   }
+
+  display(){
+
+    fill(this.color);
+    rect(this.x, this.y, this.size, this.size);
+    text(this.name, this.x, this.y - 10);
+
+    if (this.overlay){
+      fill(127,127);
+      rect(this.x, this.y, this.size, this.size);
+    }
+  }
+
+  check(mx, my){
+    if (mx>this.x && mx<this.x + this.size && my> this.y && my<this.y + this.size){
+      this.overlay = true;
+      return true;
+    }  else {
+      this.overlay = false;
+      return false;
+    }
+  }
+
+
+
 }
